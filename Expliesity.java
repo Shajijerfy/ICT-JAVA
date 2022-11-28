49e495e6a39e535e837d6741858a72e3f24bfeda
Shajijerfy <118263198+Shajijerfy@users.noreply.github.com>
2022-11-28 20:13:20 +0530

    Add file

---
+++ Expliesity.java
@@ -0,0 +1,7 @@
+public class Expliesity {
+    public static void main(String[] args) {
+        int value = 1000;
+        int value1 = (int) value;
+        System.out.println("the value is" + value);
+    }
+}
