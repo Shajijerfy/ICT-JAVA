4f0f7dfb82f0b4b4096386cfdad9ae45c5d28e82
Shajijerfy <118263198+Shajijerfy@users.noreply.github.com>
2022-11-28 20:14:11 +0530

    Add file

---
+++ Adding file.java
@@ -0,0 +1,10 @@
+public class Adding file
+{
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 200;
+        int sum = number1 + number2;
+        System.out.println("The sum is" + sum);
+    }
+}
