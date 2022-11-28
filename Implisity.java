057115e30c16fcdee01d891fe5cfc1e0ba639e98
Shajijerfy <118263198+Shajijerfy@users.noreply.github.com>
2022-11-28 20:12:42 +0530

    Add file

---
+++ Implisity.java
@@ -0,0 +1,7 @@
+public class Implisity {
+    public static void main(String[] args) {
+        int value = 1000;
+        Double value = value;
+        System.out.println("the value is" + value);
+    }
+}
