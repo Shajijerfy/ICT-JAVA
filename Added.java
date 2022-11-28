aac1650ac59fccb1995de5828011d21359006458
Shajijerfy <118263198+Shajijerfy@users.noreply.github.com>
2022-11-28 20:14:44 +0530

    Add file

---
+++ Added.java
@@ -0,0 +1,11 @@
+public class Added {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 100;
+        int number3 = 150;
+        int number4 = 200;
+        int number5 = 250;
+        int sum = number1 + number2 + number3 + number4 + number5;
+        System.out.println("the sum is + sum");
+    }
+}
