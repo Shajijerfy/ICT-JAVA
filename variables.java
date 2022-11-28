53f92c0c0cb37d7c6042fe1d017eb5ceef07a1f7
Shajijerfy <118263198+Shajijerfy@users.noreply.github.com>
2022-11-28 20:04:30 +0530

    Add file

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    String name;
+    static String qualification = "B.E";
+
+    void message() {
+        String name = "shajikumar";
+        System.out.println(name);
+    }
+}
